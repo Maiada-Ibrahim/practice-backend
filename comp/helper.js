52916208402f46c75a11d721654e42d2cca9bca6
maiada-ibrahim <maiada.ibrahim.27@icloud.com>
2021-09-05 02:01:39 +0300

    finishbackend

---
+++ comp/helper.js
@@ -0,0 +1,118 @@
+
+const mongoose = require('mongoose');
+const axios = require('axios');
+
+
+module.exports = { test, handlegetalldata, handleadduserdata, handledeletedata,update }
+
+
+const photoSchema = new mongoose.Schema({
+    title: String,
+    thumb: String,
+    alt_description: String,
+    id: String,
+    email: String
+});
+const photomodel = mongoose.model('photomodel', photoSchema);
+
+
+
+function test(req, res) {
+    res.send("hello world!");
+}
+
+
+
+
+async function handlegetalldata(req, res) {
+    let search = req.query.search
+    // let email = req.query.email
+    console.log(search)
+    try {
+        let resp = await axios.get(`https://api.unsplash.com/search/photos?client_id=FwcJO2PQq4JjqT2VbrdGhPM7bx6y3YbXG42A0Dw65Xg&query=${search}`);
+        resp.data.results.map((value, index) => {
+            let myCar = new Photo(value.tags[0].title, value.urls.thumb, value.alt_description, value.id, '');
+            console.log(myCar);
+        })
+
+        res.send(resp.data.results);
+    } catch (err) {
+        // Handle Error Here
+        console.error(err);
+    }
+};
+
+async function handleadduserdata(req, res, next) {
+    let { title, thumb, alt_description, email } = req.body;
+    await   photomodel.create({ title, thumb, alt_description, email }).then(function (photomodel) {
+        console.log('gggggggggggggggg')
+        // res.send(photomodel);
+        
+    }).catch(next);
+
+
+    photomodel.find({ email }, function (err, userdata) {
+
+        if (err) {
+
+            console.log(err)
+        }
+        else {
+            res.send(userdata)
+        }
+    })
+
+}
+
+
+async function handledeletedata(req, res) {
+    let email = req.query.email
+    await photomodel.findOneAndDelete({ _id: req.params.id }).then(function (photomodel) {
+        // res.send(photomodel);
+    });
+
+    photomodel.find({ email }, function (err, userdata) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.send(userdata)
+        }
+    })
+}
+
+async function update(req,res,next){
+    let{ title, alt_description}=req.body
+    let email=req.query.email
+   
+    await photomodel.findOneAndUpdate({_id: req.params.id},req.body).then(async function(photomodel1){
+        await   photomodel.findOne({_id: req.params.id}).then(function(photomodel2){
+            // res.send(photomodel2);
+        });
+    });
+    photomodel.find({ email }, function (err, userdata) {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            res.send(userdata)
+        }
+    })
+}
+
+
+class Photo {
+    constructor(title, thumb, alt_description, id, email) {
+        this.title = title;
+        this.thumb = thumb;
+        this.alt_description = alt_description;
+        this.id = id;
+        this.email = email
+    }
+}
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const cors = require('cors');
+const server = express();
+server.use(cors())
+
+server.use(express.json())
+require('dotenv').config()
+
+const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost:27017/myapp');
+mongoose.connect('mongodb://maiadadb:0000@cluster0-shard-00-00.ii9w9.mongodb.net:27017,cluster0-shard-00-01.ii9w9.mongodb.net:27017,cluster0-shard-00-02.ii9w9.mongodb.net:27017/photodatabase?ssl=true&replicaSet=atlas-114hrc-shard-0&authSource=admin&retryWrites=true&w=majority');
+const PORT = process.env.PORT ;
+const{test,handlegetalldata,handleadduserdata,handledeletedata,update}=require('./comp/helper')
+
+
+
+
+server.get("/test", test);
+server.get("/getalldata", handlegetalldata);
+server.post("/adduserdata", handleadduserdata);
+server.delete('/deletedata/:id', handledeletedata);
+server.put('/update/:id',update)
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+server.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+  });
